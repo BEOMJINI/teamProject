7fcb2f561f3b8ba48efaee7a448c313ae7e45749
KIMBEOMJIN <kimbj0117@gmail.com>
2023-03-27 17:52:34 +0900

    final2
    final2

--- ProjectBook/src/main/java/com/basic/projectbook/mybatis/bookstoredb.sql
+++ ProjectBook/src/main/java/com/basic/projectbook/mybatis/bookstoredb.sql
@@ -14,21 +14,25 @@ address varchar(50) not null,
 mileage int not null default 0
 );
 
-
-select * from member;
-delete from member;
-drop table member;
-
-
 INSERT INTO member (name, id, pw, email, phone, interest, address) VALUES
 ('qwer', 1, 3, 'a@a.com','010-1111-1111', '추리', '경기도'),
 ('test', 2, 3, 'a@a.com','010-1111-1111', '추리', '경기도'),
 ('관리자', 'admin', 'admin', 'admin@admin.com','010-1111-1111', '관리자', '관리자');
 
+create table book(
+no int auto_increment primary key,
+title varchar(500) not null,
+image varchar(2000) not null,
+author varchar(30) not null,
+discount varchar(50) not null,
+publisher varchar(30) not null,
+pubdate varchar(50) not null,
+isbn varchar(50) unique not null,
+description varchar(5000) not null,
+genre varchar(30) not null,
+country varchar(30) not null
+);
 
-select * from member;
-delete from member;
-drop table member;
 
 
 create table cart(
@@ -40,9 +44,6 @@ id varchar(50),
 foreign key(id) references member (id) on update cascade on delete cascade,
 foreign key(isbn) references book (isbn) on update cascade on delete cascade
 );
-select * from cart;
-delete from cart;
-drop table cart;
 
 
 create table orderlist(
@@ -58,49 +59,12 @@ orderdate varchar (100) not null
 );
 
 
-
-
-select * from orderlist;
-delete from orderlist;
-drop table orderlist;
-
-
 create table sale(
 no int,
 isbn varchar(50),
 qty int
 );
 
-select * from sale;
-delete from sale;
-drop table sale;
-
-
-
-
-
-
-
-
-create table book(
-no int auto_increment primary key,
-title varchar(500) not null,
-image varchar(2000) not null,
-author varchar(30) not null,
-discount varchar(50) not null,
-publisher varchar(30) not null,
-pubdate varchar(50) not null,
-isbn varchar(50) unique not null,
-description varchar(5000) not null,
-genre varchar(30) not null,
-country varchar(30) not null
-
-
-);
-
-select * from book;
-delete from book;
-drop table book;
 
 create table review(
 no int auto_increment primary key,
@@ -111,34 +75,21 @@ title varchar(50),
 comment varchar(300),
 foreign key(id) references member (id) on update cascade on delete cascade,
 foreign key(isbn) references book (isbn) on update cascade on delete cascade
-)
+);
 
 
-
-
-select * from review;
-delete from review;
-drop table review;
-insert into review (isbn,id,rating,title,comment) values(#{isbn},#{id},#{rating},#{title},#{comment})
 
 create table store(
  no int auto_increment primary key,
  storename varchar(50) unique,
  storeid int unique
- 
-
- 
 );
-
-select * from store;
-delete from store;
-drop table store;
 
 INSERT INTO store(storename,storeid) VALUES
 ('책방 강남점(본점)',0),
 ('책방 잠실점',1),
 ('책방 목동점',2),
-('책방 영등포점',3)
+('책방 영등포점',3);
 
 create table storemap(
 no int auto_increment primary key,
@@ -156,15 +107,7 @@ insert into storemap(storeid,storename,location1,location2,img,address) values
 (0,'책방 강남점(본점)',37.503715,127.024135,'https://lh3.googleusercontent.com/p/AF1QipOs80RMKkMbTOJhnm1XWgYXd5jjLOEeC5ILvIdP=s680-w680-h510','서울특별시 서초구 강남대로 465'),
 (1,'책방 잠실점',37.514255,127.101273,'https://lh3.googleusercontent.com/p/AF1QipNDsHnEuwO-kzpkwGHphDIM5CJB8XOZI-S31gfy=s680-w680-h510','서울특별시 송파구 올림픽로 269 롯데캐슬골드 B1F'),
 (2,'책방 목동점',37.528240,126.874982,'https://lh3.googleusercontent.com/p/AF1QipPJADbMLjnhjmTpQGs10W6_VA6MrNsVzAuB3Nv2=s680-w680-h510','서울특별시 양천구 목동서로 159-1'),
-(3,'책방 영등포점',37.517014,126.904143,'https://lh3.googleusercontent.com/p/AF1QipPrfQJGDPn2ZbKihgFHd9Hpwajm6rO4v6cI3D8j=s680-w680-h510','서울특별시 영등포구 영중로 15')
-
-select * from storemap;
-delete from storemap;
-drop table storemap;
-SELECT * FROM book
-
-
-
+(3,'책방 영등포점',37.517014,126.904143,'https://lh3.googleusercontent.com/p/AF1QipPrfQJGDPn2ZbKihgFHd9Hpwajm6rO4v6cI3D8j=s680-w680-h510','서울특별시 영등포구 영중로 15');
 
 
 create table bookstock(
@@ -176,10 +119,6 @@ foreign key(isbn) references book(isbn) on update cascade on delete cascade,
 foreign key(storeid)references store(storeid) on delete cascade
 );
 
-select * from bookstock;
-delete from bookstock;
-drop table bookstock;
-
 create table applyrestock(
 no int auto_increment primary key,
 title varchar(500) not null,
@@ -190,24 +129,31 @@ storename varchar(50) not null,
 storeid int not null,
 foreign key(isbn) references book(isbn) on update cascade on delete cascade,
 foreign key(storeid) references store(storeid) on delete cascade
-)
+);
+
+
+select * from member;
+select * from book;
+
+select * from cart;
+select * from orderlist;
+select * from sale;
+select * from review;
+select * from store;
+
+select * from storemap;
+select * from bookstock;
 
 select * from applyrestock;
-delete from applyrestock;
-drop table applyrestock;
-
-
-
-
-
 
 drop table member;
+drop table book;
+
 drop table cart;
 drop table orderlist;
 drop table sale;
-drop table book;
 drop table review;
 drop table store;
+drop table storemap;
 drop table bookstock;
 drop table applyrestock;
-drop table storemap;
